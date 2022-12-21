90fda93f3c913df3196b5bcbbc46276ea0751943
Satvik Ms <satvik.ms@intimetec.com>
2022-12-21 20:42:48 +0530

    small push

---
+++ server/server.ts
@@ -0,0 +1,7 @@
+// const promptSync = require('prompt-sync');
+// const prompter = promptSync();
+
+// let userName = prompter("Enter the User Name: ");
+// let password = prompter("Enter the Password: ");
+// console.log(userName,password,'////////////////////////////////')
+
