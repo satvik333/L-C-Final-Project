5bfe07449c22ba4337d43cd6f8eecf463a83f847
Satvik Ms <satvik.ms@intimetec.com>
2023-02-01 15:45:02 +0530

    Basic Structure

--- server/server.ts
+++
@@ -1,7 +0,0 @@
-// const promptSync = require('prompt-sync');
-// const prompter = promptSync();
-
-// let userName = prompter("Enter the User Name: ");
-// let password = prompter("Enter the Password: ");
-// console.log(userName,password,'////////////////////////////////')
-
