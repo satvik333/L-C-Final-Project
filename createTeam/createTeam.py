48d2c80b1a780993db7167748a90d2626bf02a71
Satvik Ms <satvik.ms@intimetec.com>
2023-02-02 16:50:59 +0530

    some changes

--- createTeam/createTeam.py
+++ createTeam/createTeam.py
@@ -12,32 +12,37 @@ def readJsonFile(filePath):
 
 
 def createTeam():
+    # filePath = input("Enter the file path ")
     inputFileData = readJsonFile('C:/Users/satvik.ms/Desktop/final project diagrams/TeamsInputJSON.json')
     Game.gameType = inputFileData['gameType']
     playersDetails = inputFileData['players']
+    noOfMembers = 0
     if (Game.gameType == 1):
         TeamList.total = int(len(playersDetails)/11)
+        noOfMembers = 11
     elif (Game.gameType == 2):
         TeamList.total = int(len(playersDetails)/2)
+        noOfMembers = 2
     else:
         TeamList.total = int(len(playersDetails))
+        noOfMembers = 1
     playersList = []
     for i in range(TeamList.total):
         data = {
             "id": i + 1,
             "name": "Team - " + str(i),
             "gameType": Game.gameType,
-            "players": playersDetails[0: TeamList.total]
+            "players": playersDetails[0: noOfMembers]
         }
         playersList.append(data)
-        del playersDetails[0: TeamList.total]
+        del playersDetails[0: noOfMembers]
     TeamList.items = playersList
     teamData = {}
     teamData["teams"] = TeamList.items
     teamData["total"] = TeamList.total
-    return teamData
+    return json.dumps(teamData)
     
-
-print(createTeam())
+createdTeamDetails = createTeam()
+print(createdTeamDetails)
 
 
