a8c3cd01d4fc4a39c94ccf2dd83de73e7e49db1a
Satvik Ms <satvik.ms@intimetec.com>
2023-02-05 10:37:47 +0530

    goode progress

--- createTeam/client.py
+++ createTeam/client.py
@@ -8,6 +8,7 @@ client_socket.connect(server_address)
 
 with open("C:/Users/satvik.ms/Desktop/L-C-Final-Project/TeamsInputJSON.json", "r") as f:
     data = json.load(f)
+
 message = json.dumps(data).encode()
 client_socket.sendall(message)
 
--- createTeam/server.py
+++ createTeam/server.py
@@ -1,16 +1,22 @@
 import socket
+import json
+import threading
 from CreateTeam import createteam
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def handle_client(client_connection, client_address):
+    data = client_connection.recv(1024)
+    data = json.loads(data.decode())
+    response = createteam.createTeam(data)
+    response = json.dumps(response).encode()
+    client_connection.sendall(response)
+    client_connection.close()
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('localhost', 3000)
 server_socket.bind(server_address)
-
 server_socket.listen(1)
 
 while True:
     client_connection, client_address = server_socket.accept()
-    data = client_connection.recv(1024)
-    response = createteam.createTeam(data)
-    client_connection.sendall(response)
-    client_connection.close()
+    client_thread = threading.Thread(target=handle_client, args=(client_connection, client_address))
+    client_thread.start()
